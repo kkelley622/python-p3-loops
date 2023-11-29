a0386002d97274ba33068e1ad7fa770bcf168d3c
Kevin Kelley <kkelley622@gmail.com>
2023-11-29 17:37:48 -0600

    All tests passing.'

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,25 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(f"{i}")
+        i -= 1
+        print("Happy New Year!")
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    squared_ints = [int * int for int in int_list]
+    return squared_ints
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for i in range (1, 101):
+        if ((i%3 == 0) and (i%5 == 0)):
+            print("FizzBuzz")
+        elif i%3 == 0:
+            print("Fizz")
+        elif i%5 == 0:
+            print("Buzz")
+        else:
+            print(i)
